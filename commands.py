986b1bae59beaad1b92f2380f701b5efd6d442a6
Oleksii Kukhariev <65445718+flatline-code@users.noreply.github.com>
2022-11-13 12:15:40 +0200

    add commands dict

---
+++ commands.py
@@ -0,0 +1,11 @@
+COMMANDS_DICT = {
+        'hello': greeting,
+        'exit': stop,
+        'close': stop,
+        'add_contact': add_contact,
+        'add_address': add_address, 
+        'add_phone': add_phone,
+        'add_email': add_email,
+        'add_birthday': add_birthday,
+        'show_all': show_all,
+    }
--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 from classes import AddressBook, Record
-
-COMMANDS_DICT = {
-        'hello': greeting,
-        'exit': stop,
-        'close': stop,
-        'add_contact': add_contact,
-        'add_address': add_address, 
-        'add_phone': add_phone,
-        'add_email': add_email,
-        'add_birthday': add_birthday,
-        'show_all': show_all,
-    }
+from commands import COMMANDS_DICT
 
 def input_error(handler):
     def wrapper(*args):
